99da00b7e1e05a6a9d63c79777d950deff363256
Andrew <manager@imagineit.com.au>
2018-02-20 15:44:22 +1000

    cleanup

--- example/index.js
+++ example/index.js
@@ -10,11 +10,15 @@ let streamThruMiddleware = S3StreamThru({
     s3Options: {
         accessKeyId: process.env.AWS_ACCESS_KEY || "******************",
         secretAccessKey: process.env.AWS_SECRET_KEY || "**********************",
-        region: 'us-east-1'
+        region: 'us-east-1',
+        http: {
+            timeout: 240
+        },
+        timeout: 240
     },
     s3: {
         bucket: process.env.AWS_BUCKET || '****.****.****',
-        path: '/'
+        path: 'upload/'
     },
     getFilename: function(req) {
         return req.headers.filename;
@@ -23,13 +27,23 @@ let streamThruMiddleware = S3StreamThru({
         let fileuuid = req.headers.fileuuid;
         redis.get(fileuuid, function (err, reply) {
             if (err || !reply) { return callback(err, reply); }
-            console.log(reply);
+            console.log('uploading', fileuuid, reply);
             return callback(null, JSON.parse(reply));
         });
     },
     setS3Id: function(req, data, callback) {
         let fileuuid = req.headers.fileuuid;
         redis.set('file_' + fileuuid, JSON.stringify(data), callback);
+    },
+    partEtag: function(req, callback) {
+        let fileuuid = req.headers.fileuuid;
+        let redisKey = 'parts_' + fileuuid;
+        redis.hget(redisKey, req.headers.part || 1, function(err, eTag) {
+            if (err) {
+                return callback(err);
+            }
+            return callback(err, eTag);
+        });
     },
     partCompleted: function(req, eTag, callback) {
         let fileuuid = req.headers.fileuuid;
@@ -47,6 +61,12 @@ let streamThruMiddleware = S3StreamThru({
             })
         });
     },
+    cleanup: function(req, callback) {
+        let fileuuid = req.headers.fileuuid;
+        redis.del('file_' + fileuuid);
+        redis.del('parts_' + fileuuid);
+        return callback && callback(null);
+    },
     log: console.log
 });
 
@@ -56,10 +76,12 @@ app.get('/', (req, res) => res.send('POST a file to http://localhost:3000/upload
 
 app.post('/upload', (req, res) => {
     // clean up
-    let fileuuid = req.headers.fileuuid;
-    redis.del('file_' + fileuuid);
-    redis.del('parts_' + fileuuid);
-    res.send('Chunk Upload Success!', req.headers.fileuuid, ' -> ', req.s3Path);
+    console.log('received file');
+    res.status(200).json({
+        message: 'Chunk Upload Success!',
+        fileUUID: req.headers.fileuuid,
+        s3Path: req.s3Path
+    });
 });
 
 
